055c11d0d2df49eb83608553c4cd57b9a00a8b05
Gonzalesderkaefer <149519890+Gonzalesderkaefer@users.noreply.github.com>
2025-06-02 19:34:53 +0200

    Added file utils

---
+++ fileutils/fileutils.c
@@ -0,0 +1,60 @@
+#define _XOPEN_SOURCE 500 // This is needed for strtok_r
+#ifndef FILE_UTILS_H
+#define FILE_UTILS_H
+
+// Libraries
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+
+
+
+
+
+
+
+void mkdir_r(const char *path) {
+    // Copy path to buffer on stack (for strtok_r)
+    size_t pathlen = strlen(path) + 1; // Determine length for buffer
+    char pathbuf[pathlen]; // Create buffer
+    snprintf(pathbuf, pathlen, "%s", path); // Copy into buffer
+
+    // Dynamically growing buffer
+    char *current_buffer = (char *)calloc(2,sizeof(char));
+    if (!current_buffer) { // Error checking
+        return;
+    }
+    current_buffer[0] = '/'; // Initialize with '/'
+
+    char *str, *token; // for (strtok_r)
+    str = pathbuf; // Str has to point to the buffer
+    while ((token = strtok_r(str, "/", &str)) != NULL) {
+        // Size of currentbuffer, token and slash in between
+        const size_t newlen = strlen(current_buffer) + strlen(token) + 2;
+
+        // Allocate new buffer with new length
+        char *new_buffer = (char *)calloc(newlen, sizeof(char));
+        if (!new_buffer) { // Error checking
+            free(current_buffer); // We have to free this ...
+            return;
+        }
+
+        // Copy current buffer and token into new buffer
+        snprintf(new_buffer, newlen, "%s%s/", current_buffer, token);
+
+        // We don't need the old buffer
+        free(current_buffer);
+
+
+        // Reassign old variable to new buffer
+        current_buffer = new_buffer;
+
+        // Create the dir ignore when it does not
+        mkdir(current_buffer, 0755);
+    }
+
+    free(current_buffer);
+}
+#endif // FILE_UTILS_H
---
+++ fileutils/main.c
@@ -0,0 +1,6 @@
+#include "fileutils.c"
+
+
+int main(void) {
+    mkdir_r("/home/testuser/vim/");
+}
