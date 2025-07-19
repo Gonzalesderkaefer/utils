c3f065288f09a37b5492fc6479865dda7911a371
Gonzales Der Kaefer <149519890+Gonzalesderkaefer@users.noreply.github.com>
2025-07-19 15:13:36 +0200

    deleted compiled test

--- test/test.c
+++ test/test.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
+#if (__STDC_VERSION__ >= 202300L)
+#define to_ptr(value) (typeof(value) [1]){value}
+#else
+#define to_ptr(type, value) (type [1]){value}
+#endif
+
+
+int print() {
+    return 0;
+}
+
+
 int main(void) {
-    int three = 3;
-    typeof(three) four = 4;
-    printf("%d\n", four);
+    printf("%p\n", to_ptr(3));
 }
