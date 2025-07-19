b8d47233106e8466d272a9c1284bc8320ff86af0
Gonzales Der Kaefer <149519890+Gonzalesderkaefer@users.noreply.github.com>
2025-07-19 15:04:32 +0200

    Added error log

--- src/jazzlog/jazzlog.h
+++ src/jazzlog/jazzlog.h
@@ -2,9 +2,16 @@
 
 // Allocation logging
 #ifdef RESOURCE_LOG
-    #define alloc_log(ptr) printf("[ALLOC]: %p in %s:%d\n",ptr, __FILE__, __LINE__);
-    #define free_log(ptr)  printf("[FREE]: %p in %s:%d\n",ptr, __FILE__, __LINE__);
+    #define alloc_log(ptr) printf("[ALLOC]: %p in %s:%d\n",ptr, __FILE__, __LINE__)
+    #define free_log(ptr)  printf("[FREE]: %p in %s:%d\n",ptr, __FILE__, __LINE__)
 #else
     #define alloc_log(ptr)
     #define free_log(ptr)
+#endif
+
+// Error logging
+#ifdef ERROR_LOG
+    #define error_log(mesg) printf("[ERR]: %s", mesg)
+#else
+    #define error_log(mesg)
 #endif
