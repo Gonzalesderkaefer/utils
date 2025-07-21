c75ab151c8ce24907c691e626879d0ffcad93c3f
Gonzales Der Kaefer <149519890+Gonzalesderkaefer@users.noreply.github.com>
2025-07-21 06:46:24 +0200

    lookup function

--- test/test.c
+++ test/test.c
@@ -1,4 +1,8 @@
+#include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../src/tree/tree.c"
 
 #if (__STDC_VERSION__ >= 202300L)
 #define to_ptr(value) (typeof(value) [1]){value}
@@ -7,11 +11,25 @@
 #endif
 
 
-int print() {
-    return 0;
-}
-
 
 int main(void) {
-    printf("%p\n", to_ptr(3));
+    int one = 1;
+    int two = 2;
+    int three = 3;
+    int four = 4;
+    int five = 5;
+    int six = 6;
+    Tree *new_tree = tree_init(malloc, free, memcmp, sizeof(int));
+    tree_insert(new_tree, &three);
+    tree_insert(new_tree, &two);
+    tree_insert(new_tree, &one);
+    tree_insert(new_tree, &four);
+    tree_insert(new_tree, &five);
+    tree_insert(new_tree, &six);
+
+    assert(&six == tree_lookup(new_tree, to_ptr(6), sizeof(int)));
+
+    //new_tree->root = balance_node(new_tree->root);
+    printf("%ld\n", get_balance(new_tree->root));
+
 }
