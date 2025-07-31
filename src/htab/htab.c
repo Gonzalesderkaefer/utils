9926ce5379da7a57c88576ec20bbf751cdcb0607
Gonzales Der Kaefer <149519890+Gonzalesderkaefer@users.noreply.github.com>
2025-07-31 21:18:02 +0200

    value will now be copied

--- src/htab/htab.c
+++ src/htab/htab.c
@@ -13,7 +13,6 @@
 typedef struct _Pair {
     size_t keylen;
     char *key; // Does not have to be a c-style string (will always be allocated)
-    bool allocated; // If value is allocated
     void *value;
 } Pair;
 
@@ -22,18 +21,13 @@ typedef struct _Pair {
 typedef struct _Htab {
     size_t length;
     size_t capacity;
-    void *(*alloc)(size_t);
-    void (*dealloc)(void *);
+    AllocFunc alloc;
+    FreeFunc dealloc;
     Pair *storage;
 } Htab;
 
 
-Htab *htab_init(AllocFunc alloc, FreeFunc dealloc) {
-    // Allocators have to be specified
-    if (alloc == NULL || dealloc == NULL) {
-        return NULL;
-    }
-
+Htab *htab_init(const AllocFunc alloc, const FreeFunc dealloc) {
     // Allocate struct
     Htab *new_table = alloc(sizeof(Htab));
     if (new_table == NULL) {
@@ -50,8 +44,8 @@ Htab *htab_init(AllocFunc alloc, FreeFunc dealloc) {
     memset(new_storage, 0, sizeof(Pair) * INIT_SIZE);
 
     // Assign struct fields
-    new_table->alloc = alloc;
-    new_table->dealloc = dealloc;
+    new_table->alloc = alloc == NULL ? malloc : alloc;
+    new_table->dealloc = dealloc == NULL ? free : dealloc;
     new_table->capacity = INIT_SIZE;
     new_table->length = 0;
     new_table->storage = new_storage;
@@ -72,7 +66,7 @@ static uint64_t fnv1a_hash(const char *key, const size_t length) {
 
 
 
-static void reinsert_after_grow(Htab *htab, void *value, void *key, size_t keylen, bool allocated) {
+static void reinsert_after_grow(Htab *htab, void *value, void *key, size_t keylen) {
     // Sanity check
     if (value == NULL || key == NULL || keylen == 0) {
         return;
@@ -88,7 +82,6 @@ static void reinsert_after_grow(Htab *htab, void *value, void *key, size_t keyle
         htab->storage[index].keylen = keylen;
         htab->storage[index].key = key;
         htab->storage[index].value = value;
-        htab->storage[index].allocated = allocated;
     } else {
         // Find different index
         uint64_t other_index = (index + 1) % htab->capacity;
@@ -99,7 +92,6 @@ static void reinsert_after_grow(Htab *htab, void *value, void *key, size_t keyle
         htab->storage[other_index].keylen = keylen;
         htab->storage[other_index].key = key;
         htab->storage[other_index].value = value;
-        htab->storage[other_index].allocated = allocated;
     }
 }
 
@@ -110,7 +102,7 @@ static void grow(Htab *htab) {
     size_t old_cap = htab->capacity;
 
     // Allocate new storage
-    Pair *new_storage = htab->alloc(old_cap * 2);
+    Pair *new_storage = htab->alloc(sizeof(Pair) * old_cap * 2);
     if (new_storage == NULL) {
         return;
     }
@@ -129,8 +121,7 @@ static void grow(Htab *htab) {
             htab,
             cur_pair.value,
             cur_pair.key,
-            cur_pair.keylen,
-            cur_pair.allocated
+            cur_pair.keylen
         );
     }
 
@@ -141,7 +132,7 @@ static void grow(Htab *htab) {
 
 
 
-void htab_insert(Htab *htab, void *value, void *key, size_t keylen, bool allocated) {
+void htab_insert(Htab *htab, void *value, void *key, size_t value_len, size_t keylen) {
     // Sanity check
     if (value == NULL || key == NULL || keylen == 0) {
         return;
@@ -161,23 +152,30 @@ void htab_insert(Htab *htab, void *value, void *key, size_t keylen, bool allocat
         // Copy key into allocated buffer
         memcpy(new_key, key, keylen);
 
+        // Allocate value
+        char *new_value = htab->alloc(keylen);
+        if (new_value == NULL) {
+            return;
+        }
+        // Copy value into allocated buffer
+        memcpy(new_value, value, value_len);
+
         htab->storage[index].keylen = keylen;
         htab->storage[index].key = new_key;
-        htab->storage[index].value = value;
-        htab->storage[index].allocated = allocated;
+        htab->storage[index].value = new_value;
     } else {
         // Find different index
         uint64_t other_index = (index + 1) % htab->capacity;
         while (htab->storage[other_index].value != NULL) {
-            // If we reached the original index, we have
-            // to grow
+            // If we reached the original index, we have to grow
             if (other_index == index) {
                 grow(htab);
-                other_index = index;
+                //other_index = index;
             }
             // increment other_index
             other_index = (other_index + 1) % htab->capacity;
         }
+
         // Allocate key
         char *new_key = htab->alloc(keylen);
         if (new_key == NULL) {
@@ -186,10 +184,17 @@ void htab_insert(Htab *htab, void *value, void *key, size_t keylen, bool allocat
         // Copy key into allocated buffer
         memcpy(new_key, key, keylen);
 
+        // Allocate value
+        char *new_value = htab->alloc(keylen);
+        if (new_value == NULL) {
+            return;
+        }
+        // Copy value into allocated buffer
+        memcpy(new_value, value, value_len);
+
         htab->storage[other_index].keylen = keylen;
         htab->storage[other_index].key = new_key;
-        htab->storage[other_index].value = value;
-        htab->storage[other_index].allocated = allocated;
+        htab->storage[other_index].value = new_value;
     }
 }
 
@@ -241,15 +246,15 @@ void htab_free(Htab *htab) {
     for (int i = 0; i < htab->length; ++i) {
         // Check if Slot is occupied
         if (htab->storage[i].value != NULL) {
-            // Free key
+            // Free keys and values
+            htab->dealloc(htab->storage[i].key);
             htab->dealloc(htab->storage[i].value);
-            // Check if value is allocated 
-            if (htab->storage[i].allocated) {
-                // Free value
-                htab->dealloc(htab->storage[i].value);
-            }
         }
     }
+
+    // Free storage
+    htab->dealloc(htab->storage);
+
     // Save pointer to deallocation function
     void (*dealloc)(void *) = htab->dealloc;
 
